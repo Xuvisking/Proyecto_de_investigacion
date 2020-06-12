2ce1108184847545a22eeadc03d5c0a12ddee365
JoseArteaga <jose.arteaga@mail.udp.cl>
2020-06-12 01:34:37 -0400

    vista login

--- proyecto/src/app/app-routing.module.ts
+++ proyecto/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { InicioComponent } from './Componentes/inicio/inicio.component';
+import { LoginComponent } from './Componentes/login/login.component';
 
 const routes: Routes = [
   {path:"",
-  component:InicioComponent}
+  component:InicioComponent},
+  {
+    path:"login",
+  component:LoginComponent}
 ];
 
 @NgModule({
