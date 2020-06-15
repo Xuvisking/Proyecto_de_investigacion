5d2302568c3648b14993b5bb98b672b64826ba13
JoseArteaga <jose.arteaga@mail.udp.cl>
2020-06-15 01:07:51 -0400

    Api_Viajes

--- API/servidor/database.js
+++ API/servidor/database.js
@@ -1,27 +1,27 @@
 //conexion con la base de datos
 
-const mysql=require('mysql');
+const mysql = require('mysql');
 
 
 // retorna un objeto que se guarda en mysqlConecction
 const mysqlConnection = mysql.createConnection({
     //modificar con la informacion de la base de datos.
-    host:'',
-    user:'',
-    password:'',
-    database:'',
+    host: 'localhost',
+    user: 'root',
+    password: 'julieta8810',
+    database: 'Proyecto_de_investigacion',
     port: 3306
 });
 
 //establecer el conexion
-mysqlConnection.connect(function(err){
+mysqlConnection.connect(function(err) {
 
-    if(err){
+    if (err) {
         console.log(err);
         return;
-    }else{
+    } else {
         console.log('DB is connected!');
     }
 });
 
-module.exports=mysqlConnection;// para poder exportar y trabajar con a conexion de la base de datos
+module.exports = mysqlConnection; // para poder exportar y trabajar con a conexion de la base de datos
