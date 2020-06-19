b899ed21f8f0cf75025a3084dc29343ddd3f5c4f
JoseArteaga <jose.arteaga@mail.udp.cl>
2020-06-18 23:46:37 -0400

    viajes funcionando

--- Frontend/src/app/Componentes/viajes/viajes.component.ts
+++ Frontend/src/app/Componentes/viajes/viajes.component.ts
@@ -6,12 +6,18 @@ import {EventosService} from '../../services/eventos.service';
   styleUrls: ['./viajes.component.sass']
 })
 export class ViajesComponent implements OnInit {
-
+  
+  viajes: any=[];
   constructor(private eventos:EventosService) { }
+  
 
   ngOnInit(): void {
     this.eventos.getViajesProyecto(1).subscribe(
-      res=>console.log(res),
+      res=>{
+        this.viajes=res;
+        console.log(this.viajes)
+      },
+      
       err=>console.log(err)
     )
 
