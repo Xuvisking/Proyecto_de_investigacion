6e5e7694eeb07f28781eeec6caea0c18da094fe6
Xuvisking <vicente.lopez@mail.udp.cl>
2020-06-28 04:47:26 -0400

    Cambios principales en home

--- Frontend/src/app/Componentes/home/home.component.ts
+++ Frontend/src/app/Componentes/home/home.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { documento } from '../../models/home';
+
+import { HomeService } from "../../services/home.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  aux: object;
+  localusarid: number;
+  cantidad_de_proyectos =[];
+  datos_de_los_proyectos: any =[];
+  bool:boolean=true;
+
+  datosproyectos : documento = {
+    "Proy_ID": null,
+    "Nombre": null,
+    "Descripcion": null,
+    "Estado": null
+  }
+
+  constructor(private iniciohome:HomeService, private router:Router) { }
 
   ngOnInit(): void {
+    this.localusarid = Number(localStorage.getItem("User_ID"))
+    this.iniciohome.retornarproyectos(this.localusarid).subscribe(
+      res => {
+        this.aux = res;
+        console.log(this.aux);
+        for (let index = 0; index < Object.keys(this.aux).length; index++) {
+          this.iniciohome.retornardatosproyecto(Number(this.aux[index].Proyecto_Proy_ID)).subscribe(
+            res => {
+              this.datos_de_los_proyectos.push(res[0]);
+              console.log(this.datos_de_los_proyectos);
+            },
+            err => console.log(err)
+          )
+          
+        }
+      },
+      err => console.log(err)
+    )
   }
 
 }
